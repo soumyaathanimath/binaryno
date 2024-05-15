786a7b05d94518bcafc77e3a4834815e4646b13c
soumyaathanimath <>
2024-05-15 16:29:48 +0530

    binary number

---
+++ src/binaryno/Binaryno.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package binaryno;
+
+/**
+ *
+ * @author dell1
+ */
+public class Binaryno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int b=100110010;
+        
+        String str=String.valueOf(b);
+        
+        System.out.println(str.matches("[11111]+"));
+        
+        
+    }
+}
+    
+   
